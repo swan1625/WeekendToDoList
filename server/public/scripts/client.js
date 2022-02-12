0ba4e39c8dd60d59e7744bcbce21e00a6a88c679
Andy Swanson <swan1625@icloud.com>
2022-02-12 14:34:57 -0600

    inputs rendering to dom

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,6 +1,7 @@
 $(document).ready(onReady);
 
 function onReady() {
+    getTask();
     $('#submit-task').on('click', handleSubmit)
 }
 
@@ -8,7 +9,7 @@ function handleSubmit() {
     console.log('is this working?');
     let toDoSend = {
         task: $('#add-task').val(),
-        status: 'not finished'
+        status: 'false'
     }
     console.log('add', toDoSend);
     $.ajax({
@@ -18,7 +19,7 @@ function handleSubmit() {
     }).then(function(response) {
         //handle response
         console.log('back from post with', response);
-        
+        getTask();
     }).catch(function(err) {
         
         alert('Error adding task. Please try again later.')    
@@ -26,5 +27,28 @@ function handleSubmit() {
     }); 
 }
 
+
+function getTask() {
+    console.log('running getTask');
+    $.ajax({
+        method: 'GET',
+        url: '/todolist'
+    }).then(function(response) {
+        console.log(response);
+        renderTasks(response);
+console.log('back from get', response);
+    }).catch(function (error) {
+        console.log('error in artist get', error);
+        alert('trouble getting items');
+    });
+}
     
-     
+function renderTasks(list) {
+    $('#task-table-body').empty();
+    for(let task of list) {
+        $('#task-table-body').append( `
+            <tr>
+                <td>${task.task}</td>
+                <td>${task.status}</td>
+            </tr>` );
+    }}
--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -7,28 +7,32 @@ router.post ('/', (req, res)=>{
     console.log('Adding Task', newTask);
    
     let queryText = `
-    INSERT INTO "tasks" ("task", "complete")
+    INSERT INTO "tasks" ("task", "status")
     VALUES ($1, $2);
     `;
 
-    pool.query(queryText, [newTask.task, newTask.complete])
+    pool.query(queryText, [newTask.task, newTask.status])
         .then((result) => {
             res.sendStatus(201);
         })
         .catch((error) => {
             console.log(`Error adding new task`, queryText, error);
-        res.sendStatus(500);
+        res.sendStatus(400);
     });
 });
-
-    
-
-
 
 
 router.get ('/', (req, res)=>{ 
     console.log('get hit');
-    res.send('bark');
+    let queryText = 'SELECT * FROM "tasks";';
+    pool.query(queryText)
+      .then((result) => {
+          res.send(result.rows);
+      })
+      .catch((err) => {
+          console.log('Error making query', queryText, err);
+          res.sendStatus(400);
+      });
 }) //
 
 
